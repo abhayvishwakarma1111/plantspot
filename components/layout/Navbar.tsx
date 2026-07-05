8690ac5f6555ce74a1d6d371bff0263b989b6320
ABHAY VISHWAKARMA <abhayvishwakarma1111@gmail.com>
2026-07-05 09:21:00 +0530

    logo

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -4,9 +4,20 @@ export default function Navbar() {
   return (
     <header className="border-b bg-white">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
-        <Link href="/" className="text-2xl font-bold text-green-700">
-          🌿 PlantSpot
-        </Link>
+        <Link
+  href="/"
+  className="flex items-center gap-3"
+>
+  <img
+  src="/pslogo1.svg"
+  alt="PlantSpot Logo"
+  className="h-10 w-auto"
+/>
+
+  <span className="text-2xl font-bold text-green-700">
+    PlantSpot
+  </span>
+</Link>
 
         <Link
           href="/register"
